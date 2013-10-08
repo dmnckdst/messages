02b8b35308450f37cdca2df681b0d9da247ec243
Jesse Waites <jesse.waites@gmail.com>
2013-10-07 22:45:00 -0500

    filled in link paths in navigation

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Messages'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Messages')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Welcome to Messages")
     end
 
     it "should have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('| Home')
     end
   end
@@ -23,12 +23,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Welcome to Messages | Help")
     end
 
@@ -37,14 +37,22 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Welcome to Messages | Help")
+      visit about_path
+      expect(page).to have_title("Welcome to Messages | About")
     end
   end
 
+  describe "Contact Page" do 
+
+  	it "should have the content 'Contact'" do 
+  		visit contact_path
+  		expect(page).to have_content('Contact')
+  		# expect(page).to have_title("Welcome to Messages | Contact")
+  	end
+  end		
 end
