91d27b06978492048553ef5ca0898b6e00620855
Jesse Waites <jesse.waites@gmail.com>
2013-10-08 20:17:17 -0500

    added password field to user table, index to user table, and validations for password, regex for email, 50 char constraint for username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Messages::Application.routes.draw do
+  get "users/new"
+
   root  'static_pages#home'
+
+  #match '/users/new', to: 'static_pages#help', via: 'get'
+
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,56 +3,51 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-
+    before { visit root_path }
     it "should have the content 'Messages'" do
-      visit root_path
       expect(page).to have_content('Messages')
     end
 
     it "should have the base title" do
-      visit root_path
       expect(page).to have_title("Welcome to Messages")
     end
 
     it "should have a custom page title" do
-      visit root_path
       expect(page).to have_title('| Home')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "should have the content 'Help'" do
-      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit help_path
       expect(page).to have_title("Welcome to Messages | Help")
     end
 
   end
 
   describe "About page" do
+    before { visit about_path }
 
     it "should have the content 'About'" do
-      visit about_path
       expect(page).to have_content('About')
     end
 
     it "should have the right title" do
-      visit about_path
       expect(page).to have_title("Welcome to Messages | About")
     end
   end
 
   describe "Contact Page" do 
+    before { visit contact_path }
 
   	it "should have the content 'Contact'" do 
-  		visit contact_path
   		expect(page).to have_content('Contact')
-  		# expect(page).to have_title("Welcome to Messages | Contact")
   	end
+  	
   end		
 end
