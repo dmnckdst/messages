257ffb7e3c8544e6c3b8a3677ab59bc8e3f05ca9
Jesse Waites <jesse.waites@gmail.com>
2013-10-10 22:09:00 -0500

    completed the authorization

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,10 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-
-//= require bootstrap
+//= require chartkick
+//= require jsapi
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
